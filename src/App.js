1673bc1920ac0068ab2122d4caec33d857408357
AKSHATH P <akshathpkk@gmail.com>
2024-05-10 19:59:18 +0530

    added accordian

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SideMenu from "./SideMenu";
+import Accordian1 from "./Accordian1";
+import Accordian2 from "./Accordian2";
+import Accordian3 from "./Accordian3";
 import { useState } from "react";
-
 
 // import myImage from "./28.png"
 function App() {
-  const [menu , togglemenu]= useState(false);
+  const [menu, togglemenu] = useState(false);
+  const [box1, togglebox1] = useState(false);
+  const [box2, togglebox2] = useState(false);
+  const [box3, togglebox3] = useState(false);
 
   return (
     <div className=" phone:ml-0 ml-16">
@@ -17,20 +22,28 @@ function App() {
           src="https://res.cloudinary.com/do4vytmxm/image/upload/v1695803892/images/Logo_ev7w4u.png"
         />
         <div className="lg:hidden  ml-4  mt-8 hover:cursor-pointer">
-          <img  src="/images/hambuger.png" onClick={()=>togglemenu(!menu)}/>
+          <img src="/images/hambuger.png" onClick={() => togglemenu(!menu)} />
         </div>
 
         {menu && (
-        <div className="absolute top-0 right-0 z-50">
-          <SideMenu />
-        </div>
-      )}
+          <div className="absolute top-0 right-0 z-50">
+            <SideMenu />
+          </div>
+        )}
 
         <div className=" rounded-lg flex justify-between ml-72 w-max items-center text-md phone:hidden">
-          <p className="ml-2 hover:cursor-pointer hover:text-blue-700 hover:font-bold">Home</p>
-          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">Flashcard</p>
-          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">Contact</p>
-          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">FAQ</p>
+          <p className="ml-2 hover:cursor-pointer hover:text-blue-700 hover:font-bold">
+            Home
+          </p>
+          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">
+            Flashcard
+          </p>
+          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">
+            Contact
+          </p>
+          <p className="ml-20 hover:cursor-pointer hover:text-blue-700 hover:font-bold">
+            FAQ
+          </p>
 
           <div className="ml-20 mr-2 hover:cursor-pointer bg-gradient-to-b from-blue-900 to-blue-500 text-white p-4 rounded-3xl w-28 h-10 flex items-center justify-center">
             Login
@@ -44,12 +57,19 @@ function App() {
           className="  phone:w-4 phone:h-4 h-5 w-5 "
           src="/images/home.png"
         />
-        <img className="ml-3 h-3 w-3 mt-[5px] " src="/images/leftarrow.png"/>{" "}
-        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700">Flashcard</p>
-        <img className="ml-3   h-3 w-3 mt-[5px] " src="/images/leftarrow.png"/>
-        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700">Mathematics</p>
-        <img className="ml-3 h-3 w-3 mt-[5px]" src="/images/leftarrow.png"/>
-        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700 "> Relations and Functions</p>
+        <img className="ml-3 h-3 w-3 mt-[5px] " src="/images/leftarrow.png" />{" "}
+        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700">
+          Flashcard
+        </p>
+        <img className="ml-3   h-3 w-3 mt-[5px] " src="/images/leftarrow.png" />
+        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700">
+          Mathematics
+        </p>
+        <img className="ml-3 h-3 w-3 mt-[5px]" src="/images/leftarrow.png" />
+        <p className="ml-3 phone:text-sm hover:cursor-pointer  hover:text-blue-700 ">
+          {" "}
+          Relations and Functions
+        </p>
       </div>
 
       {/* HEADING  */}
@@ -60,17 +80,30 @@ function App() {
       </div>
       <div className=" phone:ml-2 ml-60 mt-14">
         <div className="flex text-xl font-bold phone:text-sm ">
-          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-blue-700 underline">Study </p>
-          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">Quiz</p>
-          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">Test</p>
-          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">Game</p>
-          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">Others</p>
+          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-blue-700 underline">
+            Study{" "}
+          </p>
+          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">
+            Quiz
+          </p>
+          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">
+            Test
+          </p>
+          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">
+            Game
+          </p>
+          <p className="mx-10 hover:text-blue-700 hover:cursor-pointer hover:underline phone:mx-4 text-gray-500">
+            Others
+          </p>
         </div>
 
         {/* QUESTION CARD */}
-        <div  className="  bg-gradient-to-b from-blue-900 to-blue-500 text-white w-[712px] phone:w-[380px]   h-96 mt-8 rounded-3xl">
+        <div className="  bg-gradient-to-b from-blue-900 to-blue-500 text-white w-[712px] phone:w-[380px]   h-96 mt-8 rounded-3xl">
           <div className="flex">
-            <img src="/images/light-bulb.png" className=" hover:cursor-pointer w-8 h-8  mt-10 ml-5" />
+            <img
+              src="/images/light-bulb.png"
+              className=" hover:cursor-pointer w-8 h-8  mt-10 ml-5"
+            />
             <img
               src="/images/volume2.png"
               className=" hover:cursor-pointer w-7 h-7 phone:ml-64 ml-[600px]  mt-12"
@@ -80,7 +113,6 @@ function App() {
             <p>9 + 6 + 7x - 2x - 3</p>
           </div>
         </div>
-
 
         <div className="flex mt-10   ">
           <img
@@ -140,25 +172,56 @@ function App() {
       <div className="mt-20">
         <p className="text-4xl text-blue-700 font-bold ml-10 ">FAQ</p>
 
-        <div className="ml-10 mt-10">
+        <div className="ml-10 mt-10 mb-20">
           <div className="flex border-2 border-blue-500 w-[800px] items-center rounded-xl mt-5 phone:w-44">
             <p className="p-5">
               Can education flashcards be used for all age groups?
             </p>
-            <img className="ml-auto h-5 w-5 hover:cursor-pointer " src="/images/downarrow.png" />
+            <img
+              className="ml-auto h-5 w-5 hover:cursor-pointer "
+              onClick={() => togglebox1(!box1)}
+              src="/images/downarrow.png"
+            />
           </div>
+
+          {box1 && (
+            <div className="">
+              <Accordian1 />
+            </div>
+          )}
+
           <div className="flex border-2 border-blue-500 w-[800px] items-center rounded-xl mt-5 phone:w-44">
             <p className="p-5">
               Can education flashcards be used for all age groups?
             </p>
-            <img className="ml-auto h-5 w-5 hover:cursor-pointer " src="/images/downarrow.png" />
+            <img
+              className="ml-auto h-5 w-5 hover:cursor-pointer "
+              onClick={() => togglebox2(!box2)}
+              src="/images/downarrow.png"
+            />
           </div>
-          <div className="flex border-2 border-blue-500 w-[800px] items-center rounded-xl mt-5 mb-20 phone:w-44">
+
+          {box2 && (
+            <div className="">
+              <Accordian2 />
+            </div>
+          )}
+
+          <div className="flex border-2 border-blue-500 w-[800px] items-center rounded-xl mt-5  phone:w-44">
             <p className="p-5">
               Can education flashcards be used for all age groups?
             </p>
-            <img className="ml-auto h-5 w-5 hover:cursor-pointer " src="/images/downarrow.png" />
+            <img
+              className="ml-auto h-5 w-5 hover:cursor-pointer "
+              onClick={() => togglebox3(!box3)}
+              src="/images/downarrow.png"
+            />
           </div>
+          {box3 && (
+            <div className="">
+              <Accordian3 />
+            </div>
+          )}
         </div>
       </div>
     </div>
